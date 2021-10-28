c634b7e45d000b3a7934368bef6fa5d23cfa6688
Obeid <zobeid@UMICH.EDU>
2021-10-28 17:04:01 -0400

    next

--- AddIntegers.java
+++ AddIntegers.java
@@ -3,7 +3,7 @@ public class AddIntegers {
     public static void main(String[] args) {
 
         int first = 155;
-        int second = 67;
+        int second = 66;
         ;
 
         System.out.println("Enter two numbers: " + first + " " + second);
